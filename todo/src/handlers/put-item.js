5d2a82fbf6194249f3048f864d9712372952f237
Mark Stark <markstark@Marks-MacBook-Pro.local>
2020-10-08 14:29:55 -0500

    UPDATE: convert all mentions of SAMPLE_TABLE to TODO_TABLE, and SampleTable to ToDoListTable   update table from SimpleTable to DynamoDB table   enable TTL attribute named taskCompleted 	modified:   .aws-sam/build/getAllItemsFunction/env.json 	modified:   .aws-sam/build/getAllItemsFunction/src/handlers/get-all-items.js 	modified:   .aws-sam/build/getAllItemsFunction/src/handlers/get-by-id.js 	modified:   .aws-sam/build/getAllItemsFunction/src/handlers/put-item.js 	modified:   .aws-sam/build/getAllItemsFunction/template.yml 	modified:   .aws-sam/build/getByIdFunction/env.json 	modified:   .aws-sam/build/getByIdFunction/src/handlers/get-all-items.js   modified:   .aws-sam/build/getByIdFunction/src/handlers/get-by-id.js 	modified:   .aws-sam/build/getByIdFunction/src/handlers/put-item.js 	modified:   .aws-sam/build/getByIdFunction/template.yml 	modified:   .aws-sam/build/putItemFunction/env.json 	modified:   .aws-sam/build/putItemFunction/src/handlers/get-all-items.js 	modified:   .aws-sam/build/putItemFunction/src/handlers/get-by-id.js 	modified:   .aws-sam/build/putItemFunction/src/handlers/put-item.js 	modified:   .aws-sam/build/putItemFunction/template.yml 	modified:   .aws-sam/build/template.yaml 	modified:   env.json 	modified:   src/handlers/get-all-items.js 	modified:   src/handlers/get-by-id.js 	modified:   src/handlers/put-item.js 	modified:   template.yml

--- todo/src/handlers/put-item.js
+++ todo/src/handlers/put-item.js
@@ -5,7 +5,7 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.TODO_TABLE;
 
 /**
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
@@ -21,13 +21,13 @@ exports.putItemHandler = async (event) => {
     const body = JSON.parse(event.body)
     const id = body.id;
     const name = body.name;
-    const expdate = body.expdate;
+    const taskCompleted = body.taskCompleted;
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
         TableName : tableName,
-        Item: { id : id, name: name , expdate: expdate}
+        Item: { id: id, name: name, taskCompleted: taskCompleted}
     };
 
     const result = await docClient.put(params).promise();
