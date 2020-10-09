86a491f7622e3fd86f4c253217210c2f46aeaef9
Mark Stark <markstark@Marks-MacBook-Pro.local>
2020-10-09 13:51:48 -0500

    UPDATE: add selectTask function, update state properties, and new view 	modified:   src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
-import { Container, Row, Button, Col, ListGroup, ListGroupItem, Alert } from 'reactstrap';
+import { Container, Row, Button, Col, ListGroup, ListGroupItem, Alert, ListGroupItemText } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faEdit, faList, faDotCircle } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 class App extends Component {
   
   state = {
     isLoading : false,
-    tasks: [],
+    tasks:[],
+    renderTasks: [],
+    todoTasks: [],
     completedTasks : [],
   }
  
   updateTask(id){
 
-    const updatedTasks = [...this.state.tasks].reduce((tasks, task) => {
+    const updatedTasks = [...this.state.todoTasks].reduce((tasks, task) => {
       
       if (task.id === id) {
         tasks['completed'] = task;
-        tasks['todo'] = [...this.state.tasks].filter(task => task.id !== id);
+        tasks["todo"] = [...this.state.todoTasks].filter(
+          (task) => task.id !== id
+        );
         console.log(tasks)
         return tasks;
       } else return tasks;
       }, {});
-    this.setState({ completedTasks : [...this.state.completedTasks, updatedTasks.completed] });
-    this.setState({tasks : updatedTasks.todo});
+    this.setState({
+      completedTasks: [...this.state.completedTasks, updatedTasks.completed],
+      todoTasks: updatedTasks.todo,
+      renderTasks:updatedTasks.todo,
+    });
+  }
+
+  selectTask(id){
+    const selectedTask = [...this.state.tasks].filter(task => task.id === id);
+    console.log(selectedTask)
+    this.setState({ renderTasks: selectedTask });
+  }
+
+  getTasks(){
+    this.setState({renderTasks: this.state.todoTasks})
   }
 
   async componentDidMount(){
@@ -35,26 +52,57 @@ class App extends Component {
     console.log(repsponse);
     const todo = repsponse.filter((task) => !task.taskCompleted);
     const complete = repsponse.filter((task) => task.taskCompleted);
-    this.setState({tasks: todo, completedTasks: complete, isLoading: false});
+    this.setState({
+      tasks: repsponse,
+      renderTasks: todo,
+      todoTasks: todo,
+      completedTasks: complete,
+      isLoading: false,
+    });
   }
 
   render () {
     
     const isLoading = this.state.isLoading;
-    const allTasks = this.state.tasks;
+    const allTasks = this.state.renderTasks;
 
     if (isLoading) 
       return (<div>Loading...</div>);
 
-      let tasks = allTasks.map((task) => (
-        <ListGroupItem color="secodary" key={task.id}>
+  let tasks =
+    allTasks.length === 1 ? (
+      <ListGroupItem>
+        <Row>
+          <Col>
+            <Alert color="dark">{allTasks[0].task}</Alert>
+          </Col>
+          <Col md="1">
+            <Button
+              color="success"
+              onClick={() => this.updateTask(allTasks[0].id)}>
+              <FontAwesomeIcon icon={faCheckCircle} />
+            </Button>
+            <Button color="warning" onClick={() => this.getTasks()}>
+              <FontAwesomeIcon icon={faList} />
+            </Button>
+            <Button
+              color="info"
+              onClick={() => this.updateTask(allTasks[0].id)}>
+              <FontAwesomeIcon icon={faEdit} />
+            </Button>
+          </Col>
+        </Row>
+      </ListGroupItem>
+    ) : (
+      allTasks.map((task) => (
+        <ListGroupItem key={task.id}>
           <Row>
             <Col>
               <Alert color="dark">{task.task}</Alert>
             </Col>
             <Col md="1">
-              <Button color="warning" onClick={() => this.updateTask(task.id)}>
-                <FontAwesomeIcon icon={faEdit} />
+              <Button color="warning" onClick={() => this.selectTask(task.id)}>
+                <FontAwesomeIcon icon={faDotCircle} />
               </Button>
               <Button color="success" onClick={() => this.updateTask(task.id)}>
                 <FontAwesomeIcon icon={faCheckCircle} />
@@ -62,40 +110,50 @@ class App extends Component {
             </Col>
           </Row>
         </ListGroupItem>
-      ));
+      ))
+    );
 
       const completedTasks = this.state.completedTasks.map(task => <ListGroupItem key={task.id}><Alert color="success">{task.task}</Alert></ListGroupItem>)
 
-      return ( 
-
+      return (
         <Container>
-            <Row>
-              <Col className='text-center'>
-                <h1>Todo List</h1>
-              </Col>
-            </Row>
+          <Row>
+            <Col className="text-center">
+              <h1>Todo List</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
               <Row>
-                <Col>
-                  <Row>
-                    <Col md="6">
-                      <ListGroup className='text-center'>
-                        <h4>Tasks</h4>
-                        {this.state.tasks.length === 0 ? <ListGroupItem><Alert color="success">All Tasks Complete!</Alert></ListGroupItem> : tasks}
-                      </ListGroup>
-                    </Col>
-                    <Col md="6">
-                      <ListGroup className='text-center'>
-                        <h4>Completed Tasks</h4>
-                        {this.state.completedTasks.length === 0 ? <ListGroupItem><Alert color="danger">Get To Work!</Alert></ListGroupItem> : completedTasks}
-                      </ListGroup>
-                    </Col>
-                  </Row>
+                <Col md="6">
+                  <ListGroup className="text-center">
+                    <h4>Tasks</h4>
+                    {this.state.todoTasks.length === 0 ? (
+                      <ListGroupItem>
+                        <Alert color="success">All Tasks Complete!</Alert>
+                      </ListGroupItem>
+                    ) : (
+                      tasks
+                    )}
+                  </ListGroup>
+                </Col>
+                <Col md="6">
+                  <ListGroup className="text-center">
+                    <h4>Completed Tasks</h4>
+                    {this.state.completedTasks.length === 0 ? (
+                      <ListGroupItem>
+                        <Alert color="danger">Get To Work!</Alert>
+                      </ListGroupItem>
+                    ) : (
+                      completedTasks
+                    )}
+                  </ListGroup>
                 </Col>
               </Row>
+            </Col>
+          </Row>
 
-            <Row>
-  
-            </Row>
+          <Row></Row>
         </Container>
       );
   }
