0630893f00c73845cedebcd779768f2ecef8d5a9
Mark Stark <markstark@Marks-MacBook-Pro.local>
2020-10-10 00:49:54 -0500

    UPDATE: update layout. change button order in SelectedTaskList, set col size 	modified:   src/views/selectTaskList.js 	modified:   src/views/todoTaskList.js

--- client/src/views/selectTaskList.js
+++ client/src/views/selectTaskList.js
@@ -9,17 +9,17 @@ class SelectedTaskList extends Component {
   render() { 
     return <ListGroupItem>
       <Row>
-        <Col>
+        <Col md="11">
           <Alert color="dark">{this.props.task}</Alert>
         </Col>
         <Col md="1">
-          <Button color="success" onClick={() => this.props.updateTask(this.props.tasks, this.props.id)}>
-            <FontAwesomeIcon icon={faCheckCircle} />
-          </Button>
-          <Button color="warning" onClick={() => this.props.getTasks()}>
+          <Button size="xs" color="warning" onClick={() => this.props.getTasks()}>
             <FontAwesomeIcon icon={faList} />
           </Button>
-          <Button color="danger" onClick={() => this.props.deleteTask(this.props.id)}>
+          <Button size="xs" color="success" onClick={() => this.props.updateTask(this.props.tasks, this.props.id)}>
+            <FontAwesomeIcon icon={faCheckCircle} />
+          </Button>
+          <Button size="xs" color="danger" onClick={() => this.props.deleteTask(this.props.id)}>
             <FontAwesomeIcon icon={faTrash} />
           </Button>
         </Col>
--- client/src/views/todoTaskList.js
+++ client/src/views/todoTaskList.js
@@ -9,17 +9,18 @@ class ToDoTaskList extends Component {
   render() {
     return <ListGroupItem>
       <Row>
-        <Col>
+        <Col md="11">
         < Alert color="dark">{this.props.task}</Alert>
         </Col>
         <Col md="1">
-          <Button color="warning" onClick={() => this.props.selectTask(this.props.allTasks, this.props.id)}>
+          <Button size="xs" color="warning" onClick={() => this.props.selectTask(this.props.allTasks, this.props.id)}>
             <FontAwesomeIcon icon={faDotCircle} />
           </Button>
-          <Button color="success" onClick={() => this.props.updateTask(this.props.tasks, this.props.id)}>
+          <Button size="xs" color="success" onClick={() => this.props.updateTask(this.props.tasks, this.props.id)}>
           < FontAwesomeIcon icon={faCheckCircle} />
           </Button>
-          <Button color="danger" onClick={() => this.props.deleteTask(this.props.id)}>
+          <br></br>
+          <Button size="xs" color="danger" onClick={() => this.props.deleteTask(this.props.id)}>
             <FontAwesomeIcon icon={faTrash} />
           </Button>
         </Col>
